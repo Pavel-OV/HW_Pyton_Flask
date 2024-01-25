74c44f67824e8d346c140fb9eab8f56dd8dda76e
Pavel <black_yaguar@mail.ru>
2024-01-25 11:13:19 +0500

    Возможность задавать список URL-адресов через аргументы командной строки.

---
+++ HW_4/parse.py
@@ -0,0 +1,15 @@
+import argparse
+import os
+import time
+
+import requests
+
+
+def parse():
+    parser = argparse.ArgumentParser(description='Загружает изображения из заданного списка URL-адресов')
+    parser.add_argument('-u', '--urls', nargs='+', type=str, help='URL-адреса изображений, разделенные пробелом')
+    return parser.parse_args()
+
+
+
+ 
